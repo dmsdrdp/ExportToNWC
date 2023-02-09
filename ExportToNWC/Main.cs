7d576bb926b3182dc5c9bba6d7ddcdf3ef0707a6
Ячменев Дмитрий <dmsdrdp@gmail.com>
2023-02-09 17:04:22 +0300

    Добавьте файлы проекта.

---
+++ ExportToNWC/Main.cs
@@ -0,0 +1,28 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExportToNWC
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            Document doc = commandData.Application.ActiveUIDocument.Document;
+
+
+            var nwcOption = new NavisworksExportOptions();
+                doc.Export(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "export.nwc",  // экспорт в nwc
+                    nwcOption);
+
+
+            return Result.Succeeded;
+        }
+    }
+}
